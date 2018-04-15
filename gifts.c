58742e9bb8a31224bf3f225e090808715cee430f
sonu2192 <37955868+sonu2192@users.noreply.github.com>
2018-04-15 16:15:51 +0530

    Update gifts.c

--- gifts.c
+++ gifts.c
@@ -3,15 +3,15 @@
 #include<string.h>
 struct process
 {
-	char s_name[10];
-	int at;
+	char name[10];
+	int atime;
 	int r;
-	int totalbill;
-	int giftnum;
-	struct gname
+	int bill;
+	int tgifts;
+	struct gifts
 	{
 		char name[30];
-		int value;
+		int cost;
 	}g[10];
 }s[10];
 struct process temp;
@@ -22,35 +22,35 @@ void main()
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-		s[i].totalbill=0;
+		s[i].bill=0;
 	}
 	for(i=0;i<n;i++)
 	{
-		printf("\nEnter the name of the student:");
-		scanf("%s",&s[i].s_name);
+		printf("\Enter Student Name:");
+		scanf("%s",&s[i].name);
 		printf("\nEnter the arrival time of the student:");
-		scanf("%d",&s[i].at);
+		scanf("%d",&s[i].atime);
 		printf("\nEnter the number of the gifts:");
-		scanf("%d",&s[i].giftnum);
+		scanf("%d",&s[i].tgifts);
 		s[i].r=s[i].r;
-		for(j=0;j<s[i].giftnum;j++)
+		for(j=0;j<s[i].tgifts;j++)
 		{
 			printf("\nEnter the gift name::");
 			fflush(stdin);
 			scanf("%s",&s[i].g[j].name);
 			printf("\nEnter the price of the gift::");
-			scanf("%d",&s[i].g[j].value);
+			scanf("%d",&s[i].g[j].cost);
 		}
-		for(j=0;j<s[i].giftnum;j++)
+		for(j=0;j<s[i].tgifts;j++)
 		{
-			s[i].totalbill=s[i].totalbill+s[i].g[j].value;    //total bill 
+			s[i].bill=s[i].bill+s[i].g[j].cost;    //total bill 
 		}
 	}
 	for(k=0;k<n-1;k++)  //bubble sort
 	{
 		for(m=0;m<n-k-1;m++)
 		{
-			if(s[m].at>s[m+1].at)  //sorting according to the arrival time
+			if(s[m].atime>s[m+1].atime)  //sorting according to the arrival time
 			{
 				temp=s[m];
 				s[m]=s[m+1];
@@ -62,7 +62,7 @@ void main()
 	{
 		for(m=0;m<n-k-1;m++)
 		{
-			if((s[m].giftnum<s[m+1].at)&&(s[m].at==s[m+1].at))     //sorting of the structures with respect to number of gifts and arrival time
+			if((s[m].tgifts<s[m+1].tgifts)&&(s[m].atime==s[m+1].atime))     //sorting of the structures with respect to number of gifts and arrival time
 			{
 				
 				temp=s[m];
@@ -75,7 +75,7 @@ void main()
 	for(i=0;i<n;i++)
 	{
 		printf("\nStudent name:");
-		printf("\n%s",&s[i].s_name);
-		printf("\n\tTotal Bill:%d",s[i].totalbill);  //total bill for the items buyed by each students
+		printf("\n%s",&s[i].name);
+		printf("\n\tTotal Bill:%d",s[i].bill);  //total bill for the items buyed by each students
 	}
 }
